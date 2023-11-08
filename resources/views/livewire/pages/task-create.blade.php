d2d7e0496e089555c69c4431fb41d982e17557ce
juanC <juankamiloc93@gmail.com>
2023-11-08 10:32:48 -0500

    create pages

--- resources/views/livewire/pages/task-create.blade.php
+++ resources/views/livewire/pages/task-create.blade.php
@@ -1,3 +1,11 @@
-<div>
-    Página nueva tarea
-</div>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Nueva tarea') }}
+        </h2>
+    </x-slot>   
+    <div>
+        Página nueva tarea
+    </div>
+</x-app-layout>
+
--- resources/views/livewire/pages/task-index.blade.php
+++ resources/views/livewire/pages/task-index.blade.php
@@ -1,3 +1,10 @@
-<div>
-    Página de tareas
-</div>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Tareas') }}
+        </h2>
+    </x-slot>
+    <div>
+        Página de tareas
+    </div>
+</x-app-layout>
